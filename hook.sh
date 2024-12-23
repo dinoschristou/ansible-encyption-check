a62183464a86531a9276e59dab0265b2f2a2cc13
Dinos Christou <dinos.christou@gmail.com>
2024-12-23 11:00:12 +0000

    v1 - of check

---
+++ hook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if ( git show :vars/vault.yaml | grep -q "\$ANSIBLE_VAULT;" ); then
+echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
+else
+echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
+exit 1
